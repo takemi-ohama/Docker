9a428e9206c414371556f564f8ed702ccca11245
takemi.ohama <takemi.ohama@gmail.com>
2023-10-18 08:41:50 +0900

    conda追加

--- python/custom/sagemaker/build.sh
+++ python/custom/sagemaker/build.sh
@@ -3,6 +3,13 @@ tag=`date +%Y%m%d`01
 
 registry=hammer
 
+region=us-west-2
+profile=nyle
+private_ecr=644301431885.dkr.ecr.${region}.amazonaws.com
+quote_name=\"${name}\"
+
+set -xe
+
 #docker build --no-cache -t ${registry}/${name}:${tag} .
 docker build -t ${registry}/${name}:${tag} .
 docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
@@ -15,8 +22,9 @@ function pushDockerHub(){
 }
 
 function pushPrivateECR(){
-    aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 888777505088.dkr.ecr.ap-northeast-1.amazonaws.com
-    aws ecr create-repository --repository-name ${name}
+    aws ecr get-login-password --profile ${profile} --region ${region} | docker login --username AWS --password-stdin ${private_ecr}
+    [ -z "`aws --profile nyle --region us-west-2 ecr describe-repositories | grep ${quote_name}`" ] && \
+            aws ecr create-repository --profile ${profile} --region us-west-2 --repository-name ${name}
     docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
     docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:latest
     docker push ${private_ecr}/${name}:${tag}
@@ -24,6 +32,6 @@ function pushPrivateECR(){
     echo "pushed: ${private_ecr}/${name}:${tag}"
 }
 
-
 pushDockerHub
+pushPrivateECR
 
