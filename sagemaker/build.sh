7a59916e5f59884f33ade28cc277bbd2591e038f
takemi.ohama <takemi.ohama@gmail.com>
2023-10-28 19:09:16 +0900

    pytorch version up

--- sagemaker/build.sh
+++ sagemaker/build.sh
@@ -1,5 +1,5 @@
 name=`basename ${PWD}`
-tag=`date +%Y%m%d`01
+tag=`date +%Y%m%d`
 
 registry=hammer
 
@@ -8,16 +8,23 @@ profile=nyle
 private_ecr=644301431885.dkr.ecr.${region}.amazonaws.com
 quote_name=\"${name}\"
 
+OID=1001
+latest=latest${OID}
+
 set -xe
 
-#docker build --no-cache -t ${registry}/${name}:${tag} .
-docker build -t ${registry}/${name}:${tag} .
-docker tag ${registry}/${name}:${tag} ${registry}/${name}:latest
+if [ -n "${OID}"i ]; then 
+  docker build --no-cache --build-arg O_UID=${OID} --build-arg O_GID=${OID} -t ${registry}/${name}:${tag} .
+else
+  docker build --no-cache -t ${registry}/${name}:${tag} .
+fi
+
+docker tag ${registry}/${name}:${tag} ${registry}/${name}:${latest}
 
 function pushDockerHub(){
     docker login
     docker push ${registry}/${name}:${tag}
-    docker push ${registry}/${name}:latest
+    docker push ${registry}/${name}:${latest}
     echo "pushed: ${registry}/${name}:${tag}"
 }
 
@@ -26,9 +33,9 @@ function pushPrivateECR(){
     [ -z "`aws --profile nyle --region us-west-2 ecr describe-repositories | grep ${quote_name}`" ] && \
             aws ecr create-repository --profile ${profile} --region us-west-2 --repository-name ${name}
     docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${tag}
-    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:latest
+    docker tag ${registry}/${name}:${tag} ${private_ecr}/${name}:${latest}
     docker push ${private_ecr}/${name}:${tag}
-    docker push ${private_ecr}/${name}:latest
+    docker push ${private_ecr}/${name}:${latest}
     echo "pushed: ${private_ecr}/${name}:${tag}"
 }
 
