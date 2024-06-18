5beebe78af38a79b35d94206b64505ada1e43c70
takemi.ohama <takemi.ohama@gmail.com>
2024-06-19 07:35:36 +0900

    versionup cdk

--- awscdkv2/build.sh
+++ awscdkv2/build.sh
@@ -9,8 +9,8 @@ set -xe
 registry=hammer
 #docker buildx create --driver docker-container --node armBuilder --name armBuilder
 #docker buildx use armBuilder
-docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t ${registry}/${name}:${tag} .
-docker buildx build --platform linux/amd64,linux/arm64/v8 --push -t ${registry}/${name}:latest .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64/v8 --push -t ${registry}/${name}:${tag} .
+docker buildx build --no-cache --platform linux/amd64,linux/arm64/v8 --push -t ${registry}/${name}:latest .
 
 function pushPrivateECR(){
   aws ecr get-login-password --profile ${profile} --region ${region} | docker login --username AWS --password-stdin ${private_ecr}
